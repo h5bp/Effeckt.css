a4eddbb8708be81675bdbc11386629950155667b
Chris Coyier <chriscoyier@gmail.com>
2013-07-03 11:34:10 -0500

    adding buttons

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,8 @@ module.exports = function( grunt ) {
           'css/demo/demo.css': 'scss/demo/demo.scss',
           'css/modules/modals.css': 'scss/modules/modals.scss',
           'css/modules/modals-1.css': 'scss/modules/modals-1.scss',
-          'css/modules/modals-2.css': 'scss/modules/modals-2.scss'
+          'css/modules/modals-2.css': 'scss/modules/modals-2.scss',
+          'css/modules/buttons-1.css': 'scss/modules/buttons-1.scss'
         }
       }
     },
@@ -37,7 +38,8 @@ module.exports = function( grunt ) {
           'css/demo/demo.autoprefixed.css': ['css/demo/demo.css'],
           'css/modules/modals.autoprefixed.css': ['css/modules/modals.css'],
           'css/modules/modals-1.autoprefixed.css': ['css/modules/modals-1.css'],
-          'css/modules/modals-2.autoprefixed.css': ['css/modules/modals-2.css']
+          'css/modules/modals-2.autoprefixed.css': ['css/modules/modals-2.css'],
+          'css/modules/buttons-1.autoprefixed.css': ['css/modules/buttons-1.css']
         }
       }
     }
