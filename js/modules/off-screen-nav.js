1ef56db3dfd51d1b2366a8d0ddf838dc947ebcd6
Chris Coyier <chriscoyier@gmail.com>
2013-07-05 13:25:52 -0500

    adding one example to off screen nav

--- js/modules/off-screen-nav.js
+++ js/modules/off-screen-nav.js
@@ -1,5 +1,7 @@
 var EffecktOffScreenNav = {
 
+  nav: $("#effeckt-off-screen-nav"),
+
   init: function() {
 
     this.bindUIActions();
@@ -8,11 +10,35 @@ var EffecktOffScreenNav = {
 
   bindUIActions: function() {
 
-    // event handlers
+    $(".off-screen-nav-button").on("click", function() {
+      var type = $(this).data("modalType");
+
+      EffecktOffScreenNav.toggleNav(type);
+    });
 
   },
 
-  specificAction: function() {
+  toggleNav: function(type) {
+
+    // Show
+    if (!EffecktOffScreenNav.nav.hasClass("effeckt-off-screen-nav-show")) {
+
+      EffecktOffScreenNav.nav.addClass(type);
+
+      setTimeout(function() {
+        EffecktOffScreenNav.nav.addClass("effeckt-off-screen-nav-show");
+      }, 500);
+
+    // Hide
+    } else {
+
+      EffecktOffScreenNav.nav.removeClass("effeckt-off-screen-nav-show");
+
+      setTimeout(function() {
+        EffecktOffScreenNav.nav.removeClass(type);
+      }, 500);
+
+    }    
 
   }
 
