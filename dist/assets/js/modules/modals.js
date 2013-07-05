7960bb7665807c930d309f72156b2415ddfdd356
Chris Coyier <chriscoyier@gmail.com>
2013-07-05 08:35:12 -0500

    house cleaning extravaganza

--- dist/assets/js/modules/modals.js
+++ dist/assets/js/modules/modals.js
@@ -2,13 +2,15 @@
  * modalEffects.js v1.1.0
  * http://www.codrops.com
  *
+ * Rewritten for effeckts project
+ *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
  * 
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var Modals2 = {
+var Modals = {
 
   overlay: $('#effeckt-overlay'),
   modal: $("#effeckt-modal"),
@@ -23,12 +25,12 @@ var Modals2 = {
   bindUIActions: function() {
 
     $(".modal2-button").on("click", function() {
-      Modals2.openModal(this);
+      Modals.openModal(this);
     });
 
     // Is this a weird double binding problem with Modals1?
     $(".effeckt-modal-close").on("click", function() {
-      Modals2.closeModal(this);
+      Modals.closeModal(this);
     })
 
   },
@@ -41,9 +43,9 @@ var Modals2 = {
 
     var button = $(el);
 
-    Modals2.modalStyle = "md-effect-" + button.data("modal-type").replace(/[^0-9]/g, '');
+    Modals.modalStyle = "md-effect-" + button.data("modal-type").replace(/[^0-9]/g, '');
 
-    Modals2.modal.addClass(Modals2.modalStyle);
+    Modals.modal.addClass(Modals.modalStyle);
 
     console.log(button.data("needs-perspective"));
 
@@ -53,33 +55,33 @@ var Modals2 = {
 
     // Using timeout so we can set style first, then show it so animations/transitions work. You normally wouldn't need to do this.
     setTimeout(function() {
-      Modals2.modal.addClass("effeckt-show");
+      Modals.modal.addClass("effeckt-show");
     }, 300);
 
-    Modals2.showOverlay();
+    Modals.showOverlay();
 
   },
 
   closeModal: function(el) {
 
-    Modals2.modal.removeClass("effeckt-show");
+    Modals.modal.removeClass("effeckt-show");
     setTimeout(function() {
-      Modals2.modal.removeClass(Modals2.modalStyle);
+      Modals.modal.removeClass(Modals.modalStyle);
       $("html").removeClass("md-perspective");
     }, 300);
 
-    Modals2.hideOverlay();
+    Modals.hideOverlay();
 
   },
 
   showOverlay: function() {
-    Modals2.overlay.addClass("effeckt-show");
+    Modals.overlay.addClass("effeckt-show");
   },
 
   hideOverlay: function() {
-    Modals2.overlay.removeClass("effeckt-show");
+    Modals.overlay.removeClass("effeckt-show");
   }
 
 }
 
-Modals2.init();
+Modals.init();
