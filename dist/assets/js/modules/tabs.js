a62e97304ecd60653144cb50d0e20ccaa7f56236
Welling Guzman <hola@wellingguzman.com>
2013-08-19 11:18:24 -0400

    add tabs

---
+++ dist/assets/js/modules/tabs.js
@@ -0,0 +1,82 @@
+/*!
+ * strength.js
+ * Original author: @aaronlumsden
+ * Further changes, comments: @aaronlumsden
+ *
+ * Rewritten for effeckts project
+ *
+ * Licensed under the MIT license
+ */
+var Tabs = {
+
+  tabsWrapClass:  '.effeckt-tabs-wrap',
+  tabsClass:      '.effeckt-tabs a.effeckt-tab',
+  tabContentClass:'.effeckt-tab-content',
+
+  init: function() {
+
+    this.initComponent();
+    this.bindUIActions();
+
+  },
+
+  initComponent: function() {
+    
+    //keep a reference to this (Tabs) object.
+    var self = this;
+
+    $(this.tabsWrapClass).each(function(){
+
+      var $el             = $(this);
+      var effect          = $el.data('effeckt-type');
+      var tabContents     = $el.find( self.tabContentClass );
+      var firstTabContent = tabContents.first();
+      var tabs            = $el.find( self.tabsClass );
+
+      tabs.removeClass('active').first().addClass('active');
+      tabContents.not(':eq(0)').addClass( 'hide-' + effect );
+
+      firstTabContent.addClass( 'show-' + effect );
+      tabContents.parent().height( firstTabContent.height() );
+
+    });
+
+  },
+
+  bindUIActions: function() {
+
+    //keep a reference to this (Tabs) object.
+    var self = this;
+
+    $(this.tabsClass).on("click", function(e) {
+      e.preventDefault();
+      self.showTab(this);
+    });
+
+  },
+
+  showTab: function(el) {
+
+    var tab         = $(el);
+    var tabsWrap    = tab.parents(this.tabsWrapClass);
+    var tabs        = tabsWrap.find( this.tabsClass );
+    var tabContents = tabsWrap.find( this.tabContentClass );
+    var effect      = tabsWrap.data('effeckt-type');
+    
+    //set active to this current clicked tab
+    tabs.removeClass('active');
+    tab.addClass('active');
+
+    var tabID = tab.attr('href');
+    var tabContent = tabContents.filter( tabID );
+
+    tabContents.removeClass('show-' + effect).addClass('hide-' + effect);
+    tabContent.addClass('show-' + effect);
+
+    //add parent height, just because of position: absolute;
+    tabContents.parent().height( tabContent.height() );
+  }
+
+};
+
+Tabs.init();
