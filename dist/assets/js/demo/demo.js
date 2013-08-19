3f17f386d3d247c654b127add4f60dd862dfa480
Chris Coyier <chriscoyier@gmail.com>
2013-08-19 14:16:55 -0500

    adding contributors

--- dist/assets/js/demo/demo.js
+++ dist/assets/js/demo/demo.js
@@ -1,12 +1,18 @@
 var EffecktDemos = {
 
+  contribURL: 'https://api.github.com/repos/h5bp/Effeckt.css/contributors?callback=?',
+
   init: function() {
 
     $(window).load(function() {
       $(".no-transitions").removeClass("no-transitions");
-      EffecktDemos.transitionEndEventName = EffecktDemos.transitionEndEventNames[Modernizr.prefixed('transition')];
-      EffecktDemos.animationEndEventName = EffecktDemos.animationEndEventNames[Modernizr.prefixed('animation')];
     });
+
+    EffecktDemos.transitionEndEventName = EffecktDemos.transitionEndEventNames[Modernizr.prefixed('transition')];
+    EffecktDemos.animationEndEventName = EffecktDemos.animationEndEventNames[Modernizr.prefixed('animation')];
+
+    this.getContributorsData();
+
   },
 
   animationEndEventNames: {
@@ -22,6 +28,34 @@ var EffecktDemos = {
     'msTransition' : 'MSTransitionEnd',
     'transition' : 'transitionend'
   },
+
+  getContributorsData: function() {
+    $.ajax({
+      type: 'GET',
+      url: EffecktDemos.contribURL,
+      async: false,
+      contentType: "application/json",
+      dataType: 'jsonp',
+      success: function(data){
+        EffecktDemos.listContributors(data.data);
+      },
+      error: function(e) {
+        console.log(e.message);
+      }
+    });
+  },
+
+  listContributors: function(data) {
+
+    var html = '';
+
+    $(data).each(function(i, user){
+      html += '<li><a href="'+ user.url.replace('api.','').replace('users/','') +'"><img src="'+ user.avatar_url +'" alt="'+ user.login +'" class="contributor-avatar"></a></li>';
+    });
+
+    $(html).appendTo('#contributors-list');
+
+  }
 
 }
 
