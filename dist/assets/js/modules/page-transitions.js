66de3a866388d852395bbba5660998ad8a8e7cb0
Welling Guzman <hola@wellingguzman.com>
2013-08-22 16:50:27 -0400

    add new implementation of page-transitions

--- dist/assets/js/modules/page-transitions.js
+++ dist/assets/js/modules/page-transitions.js
@@ -1,41 +1,74 @@
 //To Do: Add a better way to handle resetting transitions
 var EffecktPageTransitions = {
 
-  mainPage: $('#page-wrap'),
-  secondaryPage: $('#effeckt-page-transition'),
+  fromPage: '',
+  toPage: '',
   isAnimating: false,
 
   init: function() {
 
+    this.initPages();
     this.bindUIActions();
+
+  },
+
+  initPages: function(){
+
+    var $pages = $('[data-effeckt-page]');
+
+    this.fromPage = $pages.first().addClass('effeckt-page-active');
 
   },
 
   bindUIActions: function() {
 
     var self = this;
+    $('.effeckt-page-transition-button').on('click',function(e){
 
-    $('.page-transition-button').on('click',function(){
-      var transitionIn = $(this).data('effeckt-transition-in'),
-          transitionOut = $(this).data('effeckt-transition-out');
-          self.transitionPage( transitionIn, transitionOut );
+      e.preventDefault();
+
+      var transitionEffect  = $(this).data('effeckt-transition'),
+          transitionPage    = $(this).data('effeckt-transition-page');
+
+      self.transitionPage( transitionPage, transitionEffect );
+
     });
-
-    $('.page-transition-reset-button').on('click',function(){
-          self.resetTransition();
-    });
-
   },
 
-  transitionPage: function(transitionIn,transitionOut) {
+  transitionPage: function( transitionPage, transitionEffect ) {
 
-    if(this.isAnimating){
+    if ( this.isAnimating ) {
+
       return false;
+
     }
+
     this.isAnimating = true;
 
-    this.mainPage.addClass(transitionOut);
-    this.secondaryPage.addClass(transitionIn + ' effeckt-page-transition-animating');
+    // Get Pages
+    this.fromPage = $('.effeckt-page-active');
+    this.toPage = $('[data-effeckt-page="' + transitionPage + '"]');
+
+    // Set Transition Class
+    // Maybe this array class could be made using and array
+    // Relationating the In Effect with the Out effect.
+    this.toPage.addClass('effeckt-page-active');
+    this.fromPage.addClass(transitionEffect + '-out');
+    this.toPage.addClass(transitionEffect + '-in');
+
+    
+    //event trigger after animation/transition end.
+    var evt = EffecktDemos.animationEndEventName + ' ' + EffecktDemos.transitionEndEventName,
+        self= this;
+
+    this.toPage.on(evt, function () {
+
+      self.isAnimating = false;
+      self.fromPage.removeClass(transitionEffect + '-out');
+      self.fromPage.removeClass('effeckt-page-active');
+      self.toPage.removeClass(transitionEffect + '-in');
+
+    });
 
   },
   resetTransition: function() {
